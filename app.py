0aa29bfb401cde0281de7e61a901ba607ca9aae4
Hannes221 <hannespfau@gmail.com>
2024-01-18 00:05:46 +0100

    Moved templating routes to main file - works now

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app = FastAPI()
 templates_directory = os.path.join(os.getcwd(), 'templates')
 templates = Jinja2Templates(directory=templates_directory)
 
+rq_dashboard_version = "1.0" 
+
 @app.get("/", response_class=HTMLResponse)
 async def get_home(request: Request):
     return templates.TemplateResponse(
@@ -19,6 +21,43 @@ async def get_home(request: Request):
         {"request": request, "active_tab": "jobs",
          "instance_list": [], "rq_dashboard_version": "1.0"}
     )
+    
+@app.get("/workers", response_class=HTMLResponse)
+async def read_workers(request: Request):
+    worker_data = await workers.get_workers()
+    
+    active_tab = 'workers' 
+
+    return templates.TemplateResponse(
+        "workers.html",
+        {"request": request, "worker_data": worker_data, "active_tab": active_tab,
+         "instance_list": [], "rq_dashboard_version": rq_dashboard_version}
+    )
+
+@app.get("/queues", response_class=HTMLResponse)
+async def read_queues(request: Request):
+    queue_data = await queues.get_queues()
+
+    active_tab = 'queues' 
+
+    return templates.TemplateResponse(
+        "queues.html",
+        {"request": request, "queue_data": queue_data, "active_tab": active_tab,
+         "instance_list": [], "rq_dashboard_version": rq_dashboard_version}
+    )
+
+@app.get("/jobs", response_class=HTMLResponse)
+async def read_jobs(request: Request):
+    job_data = await jobs.get_jobs()
+
+    active_tab = 'jobs' 
+
+    return templates.TemplateResponse(
+        "jobs.html",
+        {"request": request, "job_data": job_data, "active_tab": active_tab,
+         "instance_list": [], "rq_dashboard_version": rq_dashboard_version}
+    )
+
 
 app.include_router(workers.router)
 app.include_router(jobs.router)
