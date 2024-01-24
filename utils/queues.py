418eeff5b082f8c342959f1ce63952cc1d9959b8
Hannes221 <hannespfau@gmail.com>
2024-01-24 20:02:55 +0100

    Fixed get_queues function

--- utils/queues.py
+++ utils/queues.py
@@ -1,12 +1,13 @@
+from redis import Redis
 from utils.redis_conn import redis
 from rq import Queue
 from utils.job_registries import get_job_registry_stats_for_queue
 
 def get_queues():
-    queue = Queue(connection=redis)
-    queues = queue.all(connection=redis)
+    queues = Queue.all(connection=Redis())
     
     return queues
+    
 
 def get_jobs_for_queue(queue_name):
     queue = Queue(queue_name, connection=redis)
