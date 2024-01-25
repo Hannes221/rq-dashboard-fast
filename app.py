12142f54eb09e7e24e470036c1d40d04a4a59097
Hannes221 <hannespfau@gmail.com>
2024-01-25 17:47:59 +0100

    Changed naming

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
-from dashboard import app as rq_dashboard_fast_router
+from dashboard import app as rq_dashboard_fast_app
 
 app = FastAPI()
 
-app.mount("", rq_dashboard_fast_router)
+app.mount("", rq_dashboard_fast_app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
