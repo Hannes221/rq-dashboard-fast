1a1b1e832da5ceaf3692c679d97919a470f88e68
Hannes221 <hannespfau@gmail.com>
2024-03-04 12:02:00 +0100

    Update version

--- rq_dashboard_fast/rq_dashboard_fast.py
+++ rq_dashboard_fast/rq_dashboard_fast.py
@@ -39,7 +39,7 @@ class RedisQueueDashboard(FastAPI):
         self.templates = Jinja2Templates(directory=templates_directory)
         self.redis_url = redis_url
 
-        self.rq_dashboard_version = "0.3.4"
+        self.rq_dashboard_version = "0.3.5"
 
         @self.get("/", response_class=HTMLResponse)
         async def get_home(request: Request):
